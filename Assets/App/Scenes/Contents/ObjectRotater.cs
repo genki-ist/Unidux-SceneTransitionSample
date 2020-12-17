300d359106d7ecff8b3656f1a3741dd8717b4c59
genki-ist <genki.toyama@i-studio.co.jp>
2020-12-17 15:32:13 +0900

    add object rotation

---
+++ Assets/App/Scenes/Contents/ObjectRotater.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotater : MonoBehaviour
+{
+    private float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var angle = this.transform.rotation.eulerAngles;
+        var elapsedTime = Time.time - this.startTime; 
+        var speed = elapsedTime * 10f;
+        angle = Vector3.one * speed;
+        var rotation = Quaternion.Euler(angle);
+        this.transform.rotation = rotation;
+    }
+}
